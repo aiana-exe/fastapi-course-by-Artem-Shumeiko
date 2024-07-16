ce5703dee198efca97951db5aeee7d7e3d8e282a
aiana-exe <aiana-exe@users.noreply.github.com>
2024-07-16 18:38:44 +0600

    after 11: cors&middleware

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
@@ -27,6 +28,18 @@ app.include_router(
 app.include_router(router_operation)
 app.include_router(router_tasks)
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 @app.on_event("startup")
 async def startup_event():
