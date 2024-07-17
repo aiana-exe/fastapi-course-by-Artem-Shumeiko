e441f571ff2e4c0f75a18452105b52bbfb1697c6
aiana-exe <aiana-exe@users.noreply.github.com>
2024-07-17 17:22:34 +0600

    after 13: websockets

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from auth.schemas import UserCreate, UserRead
 from operations.router import router as router_operation
 from tasks.router import router as router_tasks
 from pages.router import router as router_pages
+from chat.router import router as router_chat
 
 app = FastAPI(
     title="Trading App"
@@ -32,6 +33,7 @@ app.include_router(
 app.include_router(router_operation)
 app.include_router(router_tasks)
 app.include_router(router_pages)
+app.include_router(router_chat)
 
 origins = [
     "http://localhost:3000",
@@ -41,7 +43,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],  # ["*"] работает для localhost, но не на деплоее
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
                    "Authorization"],
 )
